937cc6b20085a81a6ae9ada2d5e84a2a30d1609e
Maya Shavin <6650139+mayashavin@users.noreply.github.com>
2019-10-22 21:36:02 +0300

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+export default {
+  /*
+   ** Rendering mode
+   ** Doc: https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Nuxt.js starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/api/configuration-css
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/guide/plugins
+   */
+  plugins: ["plugins/cloudinary"],
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
+  modules: [
+    // Doc: https://http.nuxtjs.org
+    "@nuxt/http",
+    // TODO: Remove it if you want to eject from codeSandbox
+    "./codesandbox"
+  ],
+
+  /*
+   ** HTTP module configuration
+   */
+  http: {
+    // See https://http.nuxtjs.org/api/#options
+  },
+
+  /*
+   ** Build configuration
+   ** Doc: https://nuxtjs.org/api/configuration-build
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <section>
+    <div class="flex">
+      <cld-image public-id="jigglypuff" width="100" height="100" cloudName="mayashavin">
+        <cld-transformation effect="grayscale" angle="45" crop="scale"/>
+      </cld-image>
+      <div>
+        <cld-context cloud-name="demo" secure=true>
+          <cld-image publicId="face_top" quality="80" />
+        </cld-context>
+      </div>
+      <cld-context cloud-name="demo" secure=true>
+        <div>
+          <cld-image public-id="face_top" crop="thumb" height="100" width="100" radius="max" gravity="auto"/>
+        </div>
+      </cld-context>
+      <cld-image publicId="vue-sdk-demo/1891680-040wigglytuff" cloudName="demo" :transformation="transformation"/>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      transformation: [{
+        width: 200,
+        height: 200,
+        crop: 'scale'
+      }, {
+        angle: '45'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  flex-wrap: nowrap;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
